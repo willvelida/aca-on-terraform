a7b2831fa49b1623f091b979456adf6c9f6e5865
willvelida <willvelida@hotmail.co.uk>
2023-02-11 21:57:12 +1300

    Adding basic container app sample to repo

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">3.40.0"
+    }
+  }
+
+  required_version = ">=1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.log_analytics_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = var.container_app_env_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = var.container_app_name
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = var.container_app_name
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+  ingress {
+    external_enabled           = true
+    allow_insecure_connections = false
+    target_port                = 80
+    traffic_weight {
+        percentage = 100
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "rg-aca-terraform"
+}
+
+variable "location" {
+  default = "australiaeast"
+}
+
+variable "log_analytics_name" {
+  default = "law-wvacaterraform"
+}
+
+variable "container_app_env_name" {
+  default = "env-wvacaterraform"
+}
+
+variable "container_app_name" {
+  default = "hello-world"
+}
